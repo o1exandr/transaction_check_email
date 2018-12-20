194193fcf1fd96e576d9d8e6afdfdb13b5e6f597
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-12-20 22:34:15 +0200

    Add project files.

---
+++ transaction_check_email/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transaction_check_email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
